7bde7065ca0edf53d9c1bf7a47db4602658099bf
danny-ch han <chenghsuan.han@gmail.com>
2019-05-08 22:36:39 +0800

    wiwimouse's version of pagination & creat page view

--- src/js/main.js
+++ src/js/main.js
@@ -1,5 +1,5 @@
 // ==========
-function appendTickets(sort) {
+function appendTickets(sort, page = 1) {
   const url = "https://jsonplaceholder.typicode.com/todos";
   let state = sort;
   if (sort !== "") {
@@ -9,78 +9,99 @@ function appendTickets(sort) {
     })
     .then(function(data) {
       console.log(`func appendTickets' state: ${state}`);
-      let html = createViewTickets(data, state);
-  
-      $('#tickets-area').append(html);
+      let { html, ticketCount } = createViewTickets(data, state, page);
+      let pagiHtml = createPaginations(ticketCount);
+
+      $('#tickets-area').html(html);
+      $('#pagination-container').html(pagiHtml);
     })
   }
 }
 
-function createViewTickets(data, state) {
-  let result = [];
-  let arrHTML = [];
-  let ticketCount = 0;
+const PageViewQty = 5;
 
-  console.log(`func createViewTickets' state: ${state}`);
-  
-  switch (state) {
-    case "open":
-      result = data
-                  .filter(ticket => ticket.completed == false)
-                  .sort((a, b) => a.title.toUpperCase() > b.title.toUpperCase() ? 1 : -1)
-                  .sort((a, b) => a.userId - b.userId);
-      
-      ticketCount = result.length;
-                
-      for (let item of result) {
-        let html = createViewTicket(item);
-        arrHTML.push(html);
-      }
-
-      
-      break;
-    case "completed":
-      result = data
-                  .filter(ticket => ticket.completed == true)
-                  .sort((a, b) => a.title.toUpperCase() > b.title.toUpperCase() ? 1 : -1)
-                  .sort((a, b) => a.userId - b.userId);
-        
-        ticketCount = result.length;
-
-        for (let item of result) {
-        let html = createViewTicket(item);
-        arrHTML.push(html);
-      }
-      break;
-    case "user":
-      result = data
-                  .sort((a, b) => a.title.toUpperCase() > b.title.toUpperCase() ? 1 : -1)
-                  .sort((a, b) => a.completed > b.completed ? 1 : -1 )
-                  .sort((a, b) => a.userId - b.userId);
-      
-      ticketCount = result.length;      
-
-      for (let item of result) {
-        let html = createViewTicket(item);
-        arrHTML.push(html);
-      }
-      break;
-    case "title":
-      result = data
-                  .sort((a, b) => a.title.toUpperCase() > b.title.toUpperCase() ? 1 : -1)
-                  .sort((a, b) => a.userId - b.userId);
-      
-      ticketCount = result.length;
-
-      for (let item of result) {
-        let html = createViewTicket(item);
-        arrHTML.push(html);
-      }
-      break;
+function createResultByStatus(data, state) {
+  switch(state) {
+    case 'open':
+    return data
+      .filter(ticket => ticket.completed == false)
+      .sort((a, b) => a.title.toUpperCase() > b.title.toUpperCase() ? 1 : -1)
+      .sort((a, b) => a.userId - b.userId);
   }
-  console.log(ticketCount)
-  // create pagination here
-  return arrHTML.join('');
+}
+
+function createViewTickets(data, state, page) {
+  const startIdx = PageViewQty * (page - 1);
+  const filtered = createResultByStatus(data, state);
+  const sliced = filtered.slice(startIdx,  startIdx + PageViewQty)
+  const ticketCount = filtered.length
+
+  return {
+    ticketCount,
+    html: sliced.map(item => createViewTicket(item)).join(''),
+  }
+
+
+  // console.log(`func createViewTickets' state: ${state}`);
+
+  // switch (state) {
+  //   case "open":
+  //     result = data
+  //                 .filter(ticket => ticket.completed == false)
+  //                 .sort((a, b) => a.title.toUpperCase() > b.title.toUpperCase() ? 1 : -1)
+  //                 .sort((a, b) => a.userId - b.userId);
+
+  //     ticketCount = result.length;
+
+  //     for (let item of result) {
+  //       let html = createViewTicket(item);
+  //       arrHTML.push(html);
+  //     }
+
+
+  //     break;
+  //   case "completed":
+  //     result = data
+  //                 .filter(ticket => ticket.completed == true)
+  //                 .sort((a, b) => a.title.toUpperCase() > b.title.toUpperCase() ? 1 : -1)
+  //                 .sort((a, b) => a.userId - b.userId);
+
+  //       ticketCount = result.length;
+
+  //       for (let item of result) {
+  //       let html = createViewTicket(item);
+  //       arrHTML.push(html);
+  //     }
+  //     break;
+  //   case "user":
+  //     result = data
+  //                 .sort((a, b) => a.title.toUpperCase() > b.title.toUpperCase() ? 1 : -1)
+  //                 .sort((a, b) => a.completed > b.completed ? 1 : -1 )
+  //                 .sort((a, b) => a.userId - b.userId);
+
+  //     ticketCount = result.length;
+
+  //     for (let item of result) {
+  //       let html = createViewTicket(item);
+  //       arrHTML.push(html);
+  //     }
+  //     break;
+  //   case "title":
+  //     result = data
+  //                 .sort((a, b) => a.title.toUpperCase() > b.title.toUpperCase() ? 1 : -1)
+  //                 .sort((a, b) => a.userId - b.userId);
+
+  //     ticketCount = result.length;
+
+  //     for (let item of result) {
+  //       let html = createViewTicket(item);
+  //       arrHTML.push(html);
+  //     }
+  //     break;
+  // }
+  // console.log(ticketCount)
+  // // create pagination here
+  // return arrHTML.join('');
 }
 
 function createViewTicket(data) {
@@ -95,21 +116,25 @@ function createViewTicket(data) {
   `
 }
 
-
-const PageViewQty = 20;
-
 function createPaginations(ticketCount) {
   let arrHTML = []
   for (let i = 1; i <= Math.ceil(ticketCount / PageViewQty); i++) {
     arrHTML.push(createPagination(i));
   }
-  return arrHTML.join('');
+  return arrHTML
+  // return arrHTML.join('');
 }
 
 function createPagination(pageNum) {
-  return `
-    <li class="pageButton" value="${pageNum}">${pageNum}</li>
-  `
+  const elm = $(`<li class="pageButton">${pageNum}</li>`);
+  elm.click(() => appendTickets(sort, pageNum));
+  return elm;
+  // console.dir(elm[0]);
+  // return elm[0].outerHTML;
+
+  // return `
+  //   <li class="pageButton" value="${pageNum}">${pageNum}</li>
+  // `
 }
 
 // tester
@@ -118,8 +143,9 @@ function createPagination(pageNum) {
 
 // ==========
 // why sometimes there's an "event" as param????
+let sort = '';
 $('#sortTickets').change(function() {
-  let sort = $(this).val();
+  sort = $(this).val();
 
   $('#tickets-area > *').remove()
 
@@ -129,5 +155,6 @@ $('#sortTickets').change(function() {
 $('.pageButton').click(function() {
   let pageNum = $(this).val();
   console.log(pageNum);
+  appendTickets(sort, pageNum);
   // create page view according to the arg
 })
